dba5e8c23aa057259047020af91a9434a26d72d3
Gheorghi Ostapenco <gheorghi.ostapenco@gmail.com>
2025-11-07 14:42:48 +0200

    fix: apply security and cost fixes to GCP infra

--- terraform-examples/bad-infra/main.tf
+++ terraform-examples/bad-infra/main.tf
@@ -1,4 +1,5 @@
 # terraform-examples/bad-infra/main.tf
+# (Now "fixed"!)
 
 terraform {
   required_providers {
@@ -14,10 +15,10 @@ provider "google" {
   zone   = "us-central1-a"
 }
 
-# 1. COST PROBLEM (for Infracost)
+# 1. COST FIX (from n2-standard-16 to e2-micro)
 resource "google_compute_instance" "expensive_vm" {
-  name         = "expensive-vm-demo"
-  machine_type = "n2-standard-16" # <-- THIS IS EXPENSIVE!
+  name         = "cheap-vm-demo" # Renamed
+  machine_type = "e2-micro"      # <-- THIS IS CHEAP!
   zone         = "us-central1-a"
 
   boot_disk {
@@ -33,16 +34,18 @@ resource "google_compute_instance" "expensive_vm" {
   tags = ["devops-auditor-demo"]
 }
 
-# 2. SECURITY PROBLEM (for Checkov)
+# 2. SECURITY FIX (from 0.0.0.0/0 to a specific IP)
 resource "google_compute_firewall" "ssh_to_world" {
-  name    = "allow-ssh-to-world"
+  name    = "allow-ssh-from-home" # Renamed
   network = "default"
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
-
-  source_ranges = ["0.0.0.0/0"] # <-- THIS IS VERY BAD!
+  
+  # We now source only from a specific "safe" IP.
+  # Checkov will no longer complain.
+  source_ranges = ["1.2.3.4/32"] # <-- THIS IS SAFE! 
   target_tags   = ["devops-auditor-demo"]
 }
